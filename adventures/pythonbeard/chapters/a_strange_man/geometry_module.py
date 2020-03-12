934ac7cea49d269a3c9654879897a77de3ade028
Segev <segev.gersh@gmail.com>
2020-03-12 14:05:31 +0200

    Added chapter 10 : a strange man

---
+++ adventures/pythonbeard/chapters/a_strange_man/geometry_module.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+from typing import List
+
+# This is a namedtuple, an name tuple is a part of the 'collections' module,
+# which contains many special collections types (as the name suggests).
+
+# A name tuple is an easy way to create a basic structure which in herits
+# from the normal tuple but, has names for each index in it.
+Point = namedtuple("Point", ["x", "y"])
+
+def center_of_polygon(points: List[Point]) -> Point:
+    """
+    Find the center of a polygon using the coordinates of each vertex.
+
+    :param points: an array of points.
+    :type points: List[Point]
+    :return: The center of given points.
+    :rtype: Point
+    """
+    # Your code here:
+    return Point(sum([point.x for point in points]) / len(points), 
+                 sum([point.y for point in points]) / len(points))
---
+++ adventures/pythonbeard/chapters/a_strange_man/main.py
@@ -0,0 +1,24 @@
+from adventures.pythonbeard.chapters.a_strange_man.geometry_module import center_of_polygon, Point
+from utils.level_key import get_hash
+
+
+triangle = [
+    Point(0,0),
+    Point(0,6),
+    Point(6,0)
+]
+
+square = [
+    Point(0,0),
+    Point(0,6),
+    Point(6,6),
+    Point(6,0)
+]
+
+if __name__ == "__main__":
+
+    match = center_of_polygon(triangle)
+
+    print("Found at : {}".format(match))
+    print("Result : {}".format(get_hash(str(match))))
+
