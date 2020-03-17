ed9cb6dba6db6c6108d34b474f8402503b9c085b
Segev <segev.gersh@gmail.com>
2020-03-17 11:23:12 +0200

    Added windows socket support

--- adventures/pythonbeard/chapters/call_the_queen/main.py
+++ adventures/pythonbeard/chapters/call_the_queen/main.py
@@ -4,9 +4,10 @@ import os.path
 
 from adventures.pythonbeard.chapters.call_the_queen.problem_solving_module import call_queen
 from adventures.pythonbeard.chapters.call_the_queen.definitions import *
+from definitions import os_name
 
 if __name__ == "__main__":
-    subprocess.Popen(["python3", (os.path.join(os.path.curdir, 
+    subprocess.Popen(["python3" if os_name == "linux" else "python", (os.path.join(os.path.curdir, 
                       "adventures/pythonbeard/chapters/call_the_queen/start_queen.py"))])
     time.sleep(1)
     call_queen(IP, PORT, MESSAGE)
